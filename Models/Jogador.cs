179152988b55e75e2de4b8631c521ceb776c93d2
carlos <carlosacgr@gmail.com>
2021-01-11 20:32:42 -0300

    Projeto Parte 1

---
+++ Models/Jogador.cs
@@ -0,0 +1,10 @@
+namespace EPlayers_AspNetCore.Models
+{
+    public class Jogador
+    {
+        //atributos da classe Jogador
+        public int IdJogador { get; set; }
+        public string Nome { get; set; }
+        public int IdEquipe { get; set; }
+    }
+}
