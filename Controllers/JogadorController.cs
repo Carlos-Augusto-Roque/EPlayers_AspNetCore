ed3bb92315f226bc4d35755739d9d83ee2634d8a
carlos <carlosacgr@gmail.com>
2021-01-15 11:35:32 -0300

    Jogador Atualizado

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -8,10 +8,10 @@ namespace EPlayers_AspNetCore.Controllers
 {
     //Rota para acesso do endereço
     [Route("Jogador")]
-    // http://localhost:5000/Equipe por exemplo
+    // http://localhost:5000/Jogador por exemplo
     public class JogadorController : Controller
     {
-        //instanciamento do objeto tipo Equipe
+        //instanciamento do objeto tipo Jogador
         Jogador jogadorModel = new Jogador();
 
         [Route("Listar")]
@@ -19,15 +19,15 @@ namespace EPlayers_AspNetCore.Controllers
         //método na qual trabalharemos com a página Index
         public IActionResult Index()
         {   
-            //Listamos todas as equipes e enviamos para a View,através da ViewBag
-            ViewBag.Equipes = jogadorModel.ReadAll();
+            //Listamos todas os jogadores e enviamos para a View,através da ViewBag
+            ViewBag.Jogadores = jogadorModel.ReadAll();
             return View();//retorna uma view,no caso , a Index
         }
         
         [Route("Cadastrar")]
 
         //método que fará a interação entre a tela(view) e o código desenvolvido
-        //receberá as informações do formulário que serão armazenadas dentro de um novo objeto (novaEquipe)
+        //receberá as informações do formulário que serão armazenadas dentro de um novo objeto (novoJogador)
         public IActionResult Cadastrar(IFormCollection form)
         {
             Jogador novoJogador = new Jogador();
@@ -35,20 +35,20 @@ namespace EPlayers_AspNetCore.Controllers
             novoJogador.Nome = form["Nome"];
          
 
-            //solicitado o método Create para salvar a novaEquipe no CSV
+            //solicitado o método Create para salvar o novoJogador no CSV
             jogadorModel.Create(novoJogador);
-            //Atualiza a lista de equipes na view
-            ViewBag.Equipes = jogadorModel.ReadAll();
+            //Atualiza a lista de jogadores na view
+            ViewBag.Jogadores = jogadorModel.ReadAll();
 
             return LocalRedirect("~/Jogador/Listar"); //redireciona para a página que se encontra
         }
 
-        //http://localhost:5000/Equipe/1
+        //http://localhost:5000/Jogador/1
         [Route("{id}")]
         public IActionResult Excluir(int id)
         {
             jogadorModel.Delete(id);
-            ViewBag.Equipes = jogadorModel.ReadAll();
+            ViewBag.Jogadores = jogadorModel.ReadAll();
             
             return LocalRedirect("~/Jogador/Listar");
         }
